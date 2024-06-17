bad33de87c78700a11d960319af9b2f2046644de
Jhon Lloyd Omblero <omblero.jhonlloyd.04@gmail.com>
2024-06-17 15:21:52 +0800

    Added Transaction

---
+++ src/personal_financial_management/Transaction.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personal_financial_management;
+
+/**
+ *
+ * @author k
+ */
+public class Transaction {
+    private int id;
+    private String type;
+    private String description;
+    private double amount;
+    
+    public Transaction(){}
+    
+    public Transaction(int id, String type, String description, double amount){
+        this.id = id;
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+}
